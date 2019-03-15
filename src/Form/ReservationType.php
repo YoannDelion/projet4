f5a002a393235f547e54373aef79d06326baa6d9
Yoann Delion <yoanndelion@gmail.com>
2019-03-15 23:15:56 +0100

    javascript : mise en place d'étapes pour le formulaire de réservation

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Reservation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -14,19 +16,19 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nombreBillets')
+            ->add('nombreBillets', ChoiceType::class, [
+                'choices' => $this->getNumbers()
+            ])
             ->add('type', ChoiceType::class, [
                 'choices' => $this->getTypes()
             ])
-//            ->add('mail')
-//            ->add('dateReservation')
-            ->add('dateVisite')
+            ->add('mail')
+            ->add('dateVisite', DateType::class)
             ->add('billets', CollectionType::class, [
                 'entry_type' => BilletType::class,
-                'allow_add'    => true,
+                'allow_add' => true,
                 'allow_delete' => true
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -36,11 +38,21 @@ class ReservationType extends AbstractType
         ]);
     }
 
-    public function getTypes(){
+    public function getTypes()
+    {
         $types = Reservation::TYPE;
         $output = [];
-        foreach ($types as $key => $value){
+        foreach ($types as $key => $value) {
             $output[$value] = $key;
+        }
+        return $output;
+    }
+
+    public function getNumbers()
+    {
+        $output = [];
+        for($i = 1; $i < 11; $i++) {
+           $output[$i] = $i;
         }
         return $output;
     }
